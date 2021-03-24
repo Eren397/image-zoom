7366da06271864e108b0d8c043e3e0893a19f2b2
Anderson <andersonwhiteguitar@gmail.com>
2021-03-24 17:57:52 -0300

    remocao console.log

--- assets/js/modules/imgZoom/getPosition.js
+++ assets/js/modules/imgZoom/getPosition.js
@@ -2,7 +2,7 @@ export function getPosition(event, image) {
     let cordinatesImage = image.getBoundingClientRect()
     let cordinateX = event.pageX - cordinatesImage.left || event.changedTouches[0].clientX - cordinatesImage.left
     let cordinateY = event.pageY - cordinatesImage.top || event.changedTouches[0].clientY - cordinatesImage.top
-    console.log(event)
+
     return {
         cordinateX,
         cordinateY
